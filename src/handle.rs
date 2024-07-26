5de089908667985d56b9e94c75a8fad6385d5847
Marvin <33938500+marvin-j97@users.noreply.github.com>
2024-07-26 20:30:34 +0200

    Update handle.rs

--- src/handle.rs
+++ src/handle.rs
@@ -1,7 +1,7 @@
 use crate::id::SegmentId;
 use std::hash::Hash;
 
-/// A value handle points into the value log.
+/// A value handle points into the value log
 #[allow(clippy::module_name_repetitions)]
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
