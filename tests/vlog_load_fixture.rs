689258e88e596712b411ca17b956a67612d2591b
marvin-j97 <marvin.janke.97@gmail.com>
2024-08-06 19:34:09 +0200

    update test fixture

--- tests/vlog_load_fixture.rs
+++ tests/vlog_load_fixture.rs
@@ -35,21 +35,3 @@ fn vlog_load_v1_corrupt() -> value_log::Result<()> {
 
     Ok(())
 }
-
-/* {
-    let mut writer = value_log.get_writer()?;
-    writer.write("a", "")?;
-    writer.write("b", "")?;
-    writer.write("c", "")?;
-    writer.write("d", "")?;
-    value_log.register_writer(writer)?;
-}
-
-{
-    let mut writer = value_log.get_writer()?;
-    writer.write("a", "We're caught between")?;
-    writer.write("b", "This life and dream")?;
-    writer.write("c", "But you and me we're bigger")?;
-    writer.write("d", "Let's try to figure this out")?;
-    value_log.register_writer(writer)?;
-} */
