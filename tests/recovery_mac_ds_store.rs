8a5ef085027230f11e134c461831441ac3f52df3
marvin-j97 <marvin.janke.97@gmail.com>
2025-02-21 19:58:34 +0100

    improve test

--- tests/recovery_mac_ds_store.rs
+++ tests/recovery_mac_ds_store.rs
@@ -1,5 +1,5 @@
 use test_log::test;
-use value_log::{Compressor, Config, IndexWriter, MockIndex, MockIndexWriter, ValueLog};
+use value_log::{Compressor, Config, ValueLog};
 
 #[derive(Clone, Default)]
 struct NoCompressor;
@@ -19,41 +19,23 @@ fn recovery_mac_ds_store() -> value_log::Result<()> {
     let folder = tempfile::tempdir()?;
     let vl_path = folder.path();
 
-    let index = MockIndex::default();
-
-    let items = ["a", "b", "c", "d", "e"];
-
     {
         let value_log = ValueLog::open(vl_path, Config::<NoCompressor>::default())?;
 
-        {
-            let mut index_writer = MockIndexWriter(index.clone());
-            let mut writer = value_log.get_writer()?;
-
-            for key in &items {
-                let value = key.repeat(10_000);
-                let value = value.as_bytes();
-
-                let key = key.as_bytes();
-
-                let vhandle = writer.get_next_value_handle();
-                index_writer.insert_indirect(key, vhandle, value.len() as u32)?;
-
-                writer.write(key, value)?;
-            }
-
-            value_log.register_writer(writer)?;
-        }
+        let mut writer = value_log.get_writer()?;
+        writer.write("a", "a")?;
+        value_log.register_writer(writer)?;
     }
 
-    {
-        std::fs::File::create(vl_path.join("segments").join(".DS_Store"))?;
-    }
+    let ds_store = vl_path.join("segments").join(".DS_Store");
+    std::fs::File::create(&ds_store)?;
+    assert!(ds_store.try_exists()?);
 
     {
         let value_log = ValueLog::open(vl_path, Config::<NoCompressor>::default())?;
         assert_eq!(1, value_log.segment_count());
     }
+    assert!(ds_store.try_exists()?);
 
     Ok(())
 }
