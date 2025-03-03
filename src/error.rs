1b8f91f035aa24d05987ad3ff40e0c86ad6a9511
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-03-03 09:41:09 -0800

    Impl Error::source

--- src/error.rs
+++ src/error.rs
@@ -39,7 +39,18 @@ impl std::fmt::Display for Error {
     }
 }
 
-impl std::error::Error for Error {}
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::Io(e) => Some(e),
+            Self::InvalidVersion(_) => None,
+            Self::Encode(e) => Some(e),
+            Self::Decode(e) => Some(e),
+            Self::Compress => None,
+            Self::Decompress => None,
+        }
+    }
+}
 
 impl From<std::io::Error> for Error {
     fn from(value: std::io::Error) -> Self {
